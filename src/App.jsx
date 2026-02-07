cc5d5d93e62889255319941cc9bd15bac81dea79
adymeda <kuldyshev.nikita@gmail.com>
2026-02-07 23:35:40 +0200

    Remove debug logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import {useTranslation} from "react-i18next"
-import i18n from "i18next"
 
 function App() {
   const { t, i18n } = useTranslation()
-  
-  console.log(i18n.store.data)
-  console.log(i18n.language)
 
   return (
     <>
